f4bb47a95fdd9f4f5d6a53e7bfa7f9259081d437
Cleiton Bueno <cleitonrbueno@gmail.com>
2014-11-06 12:19:56 -0200

    Adicionado KeyboardInterrupr no main() e action na funcao msg() para colocar as ações

--- pydd.py
+++ pydd.py
@@ -8,15 +8,15 @@ import time
 
 
 
-def msg(message):
+def msg(action,message):
     print >>sys.stderr, "*" * 20
-    print >>sys.stderr, "*" * 3, message
+    print >>sys.stderr, "*" * 3, "\033[1;31m"+action+"\033[0m",message
     print >>sys.stderr, "*" * 20
 
 
 def cmd_dd(cmd):
-    my_proc = subprocess.Popen(cmd, shell=False, stderr=subprocess.PIPE, executable=None, creationflags=0)
-    msg("PID: %s" % my_proc.pid)
+    my_proc = subprocess.Popen("/bin/dd "+cmd, shell=False, stderr=subprocess.PIPE, executable=None, creationflags=0)
+    msg("PID: ",my_proc.pid)
 
     try:
         while my_proc.poll() is None:  
@@ -34,7 +34,7 @@ def cmd_dd(cmd):
         print 'Gravado: ', output_dd_list[2]+output_dd_list[3],\
             '\tTempo decorrido: ', output_dd_list[5]+output_dd_list[6],\
             '\tTaxa: ', output_dd_list[7]+output_dd_list[8]
-     
+
     except ValueError as ex_value:
         print("Erro: %s") % (ex_value)
         my_proc.terminate()
@@ -43,18 +43,17 @@ def cmd_dd(cmd):
     except KeyboardInterrupt:
         my_proc.terminate()
         my_proc.kill()
-        exit()
+        sys.exit()
 
 
 def main():
-    cmd_line = raw_input("Entre com o comando: ")
-    
-    cmd_params = shlex.split(cmd_line)
-
-    msg("Comando + paramentos: %s" % cmd_params)
-
-    cmd_dd(cmd_params)
-
+    try:
+        cmd_line = raw_input("Entre com o comando: ")
+        cmd_params = shlex.split(cmd_line)
+        msg("Comando + paramentos: ", cmd_params)
+        cmd_dd(cmd_params)
+    except KeyboardInterrupt:
+        sys.exit()
 
 if __name__ == "__main__":
     main()
